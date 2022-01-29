7308b19ab03ad35cf7fb078bca8a6ee091eebd12
Jon Cooke <jonathanecooke90@icloud.com>
2022-01-28 23:26:30 -0700

    Added basic class for homepage in views.py, site can run now.

--- web/portfolio/views.py
+++ web/portfolio/views.py
@@ -1,3 +1,6 @@
+from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class HomePageView(generic.TemplateView):
+    template_name= 'portfolio/homepage.html'
