ac6e7ed1e4b7f6fe0c28ced4d677e51ea539a97e
Jon Cooke <jonathanecooke90@icloud.com>
2022-02-03 16:38:55 -0700

    Added forms.py with my contact form template. Added a basic class in models.py to finish later. Added classes from forms.py and models.py to views.py for context.

--- web/portfolio/models.py
+++ web/portfolio/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+# Field Choices
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+MEDIATYPE = (
+    (0,"Music"),
+    (1,"Videos"),
+    (2,"Music_Videos")
+)
+
+class BrandonMedia(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    # mediafile = 
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mediatype = models.IntegerField(choices=MEDIATYPE, default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f'{self.title}, {self.mediatype}'
--- web/portfolio/views.py
+++ web/portfolio/views.py
@@ -3,11 +3,16 @@ from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render
 from django.views import generic
 
+from .models import BrandonMedia
+from .forms import ContactForm
+
 class HomePageView(generic.TemplateView):
     template_name = 'portfolio/homepage.html'
 
 class MediaPageView(generic.ListView):
+    model = BrandonMedia
     template_name = 'portfolio/media.html'
+    context_object_name = 'media_list'
 
 class BandPageView(generic.TemplateView):
     template_name = 'bands.html'
