cef50c27b46ba7c20e5735ef0ea7b4908ab5c152
Jon Cooke <jonathanecooke90@icloud.com>
2022-02-03 16:20:38 -0700

    Added basic view classes for desired pages on views.py.

--- web/portfolio/views.py
+++ web/portfolio/views.py
@@ -1,6 +1,16 @@
+from re import template
 from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render
 from django.views import generic
 
 class HomePageView(generic.TemplateView):
-    template_name= 'portfolio/homepage.html'
+    template_name = 'portfolio/homepage.html'
+
+class MediaPageView(generic.ListView):
+    template_name = 'portfolio/media.html'
+
+class BandPageView(generic.TemplateView):
+    template_name = 'bands.html'
+
+class ContactPageView(generic.TemplateView):
+    template_name = 'contact.html'
